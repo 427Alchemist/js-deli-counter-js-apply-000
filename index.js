3a1b14aa00a5c036b4641efc00ff213091b7358f
Mike Welch <mikewelchchemist@gmail.com>
2019-01-14 19:22:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var otherDeli;
 
 function takeANumber(currentLine, newPerson) {
 
+  var i = 0;
 
-  return "Welcome, " + newPerson + " you are number " + katzDeli[i + 1] + " in line."
+  return "Welcome, " + newPerson + " you are number " + katzDeli[i + 1] + " in line.";
 }
